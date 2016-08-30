494c940cba241d9160e99b7ff0222a3f15bd9385
bfeng <fungbo@126.com>
2016-08-30 16:31:22 +0800

    [#4] - Add the script to upload image

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+password=$1
+
 echo "build dhis2 image"
 docker build -t chaimozdsd/dhis2-src:2.24 .
+
+docker login -u="chaimozdsd" -p=${password}
+docker push chaimozdsd/dhis2-src
