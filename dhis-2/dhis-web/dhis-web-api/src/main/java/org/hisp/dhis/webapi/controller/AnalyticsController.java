0223d7242d4b570d69944e0c6bce6396686df203
bracken1983 <brackenbo@hotmail.com>
2016-10-11 21:43:33 +0800

    [#10] Add exception for highlight.

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/AnalyticsController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/AnalyticsController.java
@@ -134,11 +134,15 @@ public class AnalyticsController
         contextUtils.configureResponse( response, ContextUtils.CONTENT_TYPE_JSON, CacheStrategy.RESPECT_SYSTEM_SETTING );
         Grid grid = analyticsService.getAggregatedDataValues( params, getItemsFromParam( columns ), getItemsFromParam( rows ) );
 
-        List<List<ValidationResult>> results = getValidationResults(params);
-        List<List<Object>> allRows = grid.getRows();
-        for (List<Object> row : allRows) {
-            String diseaseId = (String) row.get(0);
-            row.add(String.format("highlight.%s", verifyHighlight(results, diseaseId)));
+        try {
+            List<List<ValidationResult>> results = getValidationResults(params);
+            List<List<Object>> allRows = grid.getRows();
+            for (List<Object> row : allRows) {
+                String diseaseId = (String) row.get(0);
+                row.add(String.format("highlight.%s", verifyHighlight(results, diseaseId)));
+            }
+        } catch (Exception e) {
+            grid.setSubtitle(e.toString());
         }
 
         model.addAttribute( "model", grid );
