46e9976c88cb06c6cc7742af0bf0b28ec98ab3a2
Jiayang Yan <raymond.dream@gmail.com>
2016-10-14 02:52:09 +0800

    [#i18n] test i18n to reporting page

--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/reporting-page/scripts/components/reporting-page/index.jsx
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/reporting-page/scripts/components/reporting-page/index.jsx
@@ -97,6 +97,9 @@ class ReportingPage extends React.Component {
     };
 
     render() {
+        const d2 = this.props.d2;
+        const x = d2.i18n.getTranslation('by');
+        console.log("translate",x);
         return (
             <div className={ css.ReportingPage }>
                 <HeaderBar lastUpdate={new Date()}/>
