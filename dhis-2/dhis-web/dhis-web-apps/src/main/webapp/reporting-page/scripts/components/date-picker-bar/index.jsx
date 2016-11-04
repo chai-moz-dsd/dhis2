0e328b62b909c8435bb3aa481344583677e09eb3
Jiayang Yan <raymond.dream@gmail.com>
2016-11-04 10:58:24 +0800

    [UI] change the symbol between week and date

--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/reporting-page/scripts/components/date-picker-bar/index.jsx
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/reporting-page/scripts/components/date-picker-bar/index.jsx
@@ -21,7 +21,7 @@ export default class DatePickerBar extends React.Component {
     };
 
     render() {
-        const formatDate = (date) =>  this.context.d2.i18n.getTranslation('week') +` ${moment(date).format('ww')}` + `- ${moment(date).format('D MMMM YYYY')}`;
+        const formatDate = (date) =>  this.context.d2.i18n.getTranslation('week') +` ${moment(date).format('ww')}` + `, ${moment(date).format('D MMMM YYYY')}`;
         const { label, value, minDate, maxDate, onChange, onClean } = this.props;
 
         return (
