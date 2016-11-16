92c8ea64257bb32556f5a51d6ef0cfdbbcbf9315
bracken1983 <brackenbo@hotmail.com>
2016-11-16 13:29:13 +0800

    Remove hard code about Superuser in alluser.vm

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-user/src/main/webapp/dhis-web-maintenance-user/javascript/allUser.js
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-user/src/main/webapp/dhis-web-maintenance-user/javascript/allUser.js
@@ -1,26 +1,4 @@
-var setAllUserPermission = function (currentUserAccount){
-  $.ajax({
-    type: 'GET',
-    url: '../api/24/userRoles.json',
-    dataType: 'json',
-    username: 'admin',
-    password: 'district',
-    success: function(data)
-    {
-      $.each(data.userRoles, function(){
-        if ((this.displayName == 'Superuser')
-            && (this.id == currentUserAccount.id))
-        {
-          $('#allUserAddNew').show();
-        }
-        else
-        {
-          $('#allUserAddNew').hide();
-        }
-      });
-    }
-  });
-}
+
 
 jQuery(document).ready(function() {
   tableSorter('userList');
@@ -30,18 +8,5 @@ jQuery(document).ready(function() {
     menuItemActiveClass: 'contextMenuItemActive',
     listItemProps: ['id', 'uid', 'name', 'type', 'username']
   });
-
-  var currentAccount;
-  $.ajax({
-    type: 'GET',
-    url: '../api/24/me.json',
-    data: {fields: 'userCredentials'},
-    dataType: 'json',
-    success: function(data) {
-      setAllUserPermission(data.userCredentials.userRoles[0]);
-    }
-  });
-
-
 
 });
