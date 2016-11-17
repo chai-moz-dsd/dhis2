592f29161425c591cb233c8d18728a16ef5fed52
bracken1983 <brackenbo@hotmail.com>
2016-11-17 09:49:27 +0800

    Fix the defect of default rule problem.

--- dhis-2/dhis-web/dhis-web-validationrule/src/main/webapp/dhis-web-validationrule/javascript/general.js
+++ dhis-2/dhis-web/dhis-web-validationrule/src/main/webapp/dhis-web-validationrule/javascript/general.js
@@ -77,7 +77,7 @@ function i18nalizeAdditionalRuleType( ruleType )
 {
 	switch (ruleType) {
     case 'Default':
-        return default_rule;
+        return i18n_default_rule;
 		case 'SarampoCaseInMonths':
 			return i18n_customed_validation_rule_Sarampo;
 		case 'MeningiteIncreasedInWeeks':
