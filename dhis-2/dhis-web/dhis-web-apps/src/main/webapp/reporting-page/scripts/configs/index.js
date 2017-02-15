058b7764e1a3c1e7c6fa69c37cc6a799ade4793f
bracken1983 <brackenbo@hotmail.com>
2017-02-16 02:55:46 +0800

    [#19 -] Correct the translation in the column of ops indicator.

--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/reporting-page/scripts/configs/index.js
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/reporting-page/scripts/configs/index.js
@@ -24,7 +24,7 @@ export const syncStatusMap = {
 
 export const syncTimeStatusMap = {
     '-1': 'mBes_late_submission',
-    '0': '',
+    '0': 'null_string',
     '1': 'mBes_early_submission'
 };
 
