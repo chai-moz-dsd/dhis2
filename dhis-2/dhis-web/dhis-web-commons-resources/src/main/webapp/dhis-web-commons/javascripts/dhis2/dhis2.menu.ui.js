a0301a2f47787efb099a46f062145af9a229ffc0
wywy08271 <wywy08271@outlook.com>
2017-05-10 11:12:26 +0800

    remove account

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/dhis2/dhis2.menu.ui.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/dhis2/dhis2.menu.ui.js
@@ -1001,13 +1001,6 @@
                     description: ""
                 },
                 {
-                    name: "account",
-                    namespace: "/dhis-web-commons-about",
-                    defaultAction: "../dhis-web-commons-about/showUpdateUserAccountForm.action",
-                    icon: "../icons/function-account.png",
-                    description: ""
-                },
-                {
                     name: "help",
                     namespace: "/dhis-web-commons-about",
                     defaultAction: helpPageLink.defaultAction || "", //FIXME: This sets the help url to an empty string when the ajax call failed. We should find an alternative.
